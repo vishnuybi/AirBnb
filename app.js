03d4a916216ce3255427d04f16a8f7563865cf46
vishnu1683 <vishnushankar0011@gmail.com>
2024-08-13 01:49:54 +0530

    first commit

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
